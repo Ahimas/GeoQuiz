cbd6700c964de27169a914291307cafff447789d
Vladimir Grygoriev <vladgrygoryev@gmail.com>
2024-06-20 20:23:49 +0300

    Added second Activity

---
+++ app/src/main/java/com/vg/android/qeoquiz/CheatActivity.kt
@@ -0,0 +1,45 @@
+package com.vg.android.qeoquiz
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+private const val EXTRA_ANSWER_IS_TRUE = "com.vg.android.qeoquiz.answer_is_true"
+
+class CheatActivity : AppCompatActivity() {
+
+    private var answerIsTrue = false
+    private lateinit var showAnswerButton : Button
+    private lateinit var answerTextView : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheat)
+
+        showAnswerButton = findViewById(R.id.show_answer_button)
+        answerTextView = findViewById(R.id.answer_text_view)
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+
+        showAnswerButton.setOnClickListener {
+            val answerText = when {
+                answerIsTrue -> R.string.true_text
+                else -> R.string.false_text
+            }
+            answerTextView.setText(answerText)
+        }
+
+    }
+
+    companion object {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            }
+        }
+    }
+
+
+}
